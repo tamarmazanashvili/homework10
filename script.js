782073dd54838ecf2833888de178989a9f13a8c0
Tamar Mazanashvili <109600480+tamarmazanashvili@users.noreply.github.com>
2022-09-07 01:31:05 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Tamuna',
+    lastName: 'Mazanashvili',
+    age: '22',
+    gender: 'Female',
+    isMarried: false,
+    programmingLanguages: ['html','css'],
+
+}
+
+console.log(person.name, person.lastName, person.age, person.gender, person.isMarried, person.programmingLanguages )
